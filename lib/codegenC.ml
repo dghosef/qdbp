ef3141db8f578476c5d9780161d8a29383d5c784
Joseph Tan <jdtan638@gmail.com>
2023-05-12 00:43:43 -0700

    added c backend

---
+++ lib/codegenC.ml
@@ -0,0 +1,166 @@
+module IntMap = CollectMethods.IntMap
+module VarSet = LetBoundVariables.VarSet
+module FvSet = Refcount.FvSet
+module IntSet = Set.Make(struct type t = int let compare = compare end)
+let method_name id = "method_" ^ (string_of_int id)
+let rec indent level =
+  if level = 0 then ""
+  else
+    (indent (level - 1)) ^ "  "
+let newline level = "\n" ^ (indent level)
+let varname id = 
+  "var_" ^ (string_of_int (id))
+
+let paren s = "(" ^ s ^ ")"
+let brace s = "[" ^ s ^ "]"
+let bracket s = "{" ^ s ^ "}"
+let quoted s = 
+  "\"" ^ (String.escaped s) ^ "\""
+let fn_sig id args =
+  let args_strs = List.map (
+      fun (id, _) -> "qdbp_object_ptr " ^ (varname id)
+    ) args in
+  "qdbp_object_ptr " ^ (method_name id) ^
+  (paren
+     (String.concat ", " ("qdbp_object_arr captures" :: args_strs))
+  )
+
+let fn_declarations methods =
+  let method_strs = List.map (fun (id, (args, _, _, _, _)) -> fn_sig id args)
+      (IntMap.bindings methods) in
+  let method_strs = List.map (fun s -> s ^ ";") method_strs in
+  String.concat "\n" method_strs
+
+let invoke_fn_name cnt = "invoke_" ^ (string_of_int cnt)
+let invoke_fns methods =
+  let arities =
+    IntSet.of_list (
+      List.map (fun (_, (args, _, _, _, _)) -> List.length args) (IntMap.bindings methods)) in
+  let invoke_fn arity =
+    let args = List.init arity (fun a -> "qdbp_object_ptr arg" ^ (string_of_int a)) in
+    let args = "qdbp_object_ptr receiver" :: "label_t label" :: args in 
+    let fn_name = invoke_fn_name arity in
+    let fn_args_types = List.init arity (fun _ -> "qdbp_object_ptr") in
+    let fn_args_types = "qdbp_object_arr" :: fn_args_types in
+    let fn_args_types_str = String.concat ", " fn_args_types in
+    let fn_type = "qdbp_object_ptr(*)" ^ (paren (fn_args_types_str)) in
+    let fn_args = List.init arity (fun a -> "arg" ^ (string_of_int a)) in
+    let fn_args = "captures" :: fn_args in 
+    let fn_args_str = String.concat ", " fn_args in
+    "qdbp_object_ptr " ^ fn_name ^ (paren (String.concat ", " args)) ^ " {" ^ (newline 1) ^
+    "void* code;" ^ (newline 1) ^
+    "qdbp_object_arr captures = get_method(receiver, label, &code);" ^ (newline 1) ^
+    "return " ^ (paren ((paren fn_type) ^ "code")) ^ (paren fn_args_str) ^ ";\n}"
+  in
+  let arities_list = IntSet.elements arities in 
+  let fns = List.map invoke_fn arities_list in
+  String.concat "\n" fns
+
+let c_call fn args =
+  fn ^ (paren (String.concat ", " args))
+
+let rec expr_to_c level expr =
+  match expr with
+  | `Abort _ -> "qdbp_abort()"
+  | `Declaration (lhs, rhs, body, _, _, _) ->
+    let body_c = expr_to_c (level + 1) body in
+    let rhs_c = expr_to_c (level + 1) rhs in
+    "LET" ^ paren(
+      (varname (fst lhs)) ^ ", " ^ rhs_c ^ "," ^
+      (newline (level + 1)) ^ body_c
+    )
+  | `Drop (v, e) -> c_call "DROP" [(varname v); (expr_to_c (level + 1) e)]
+  | `Dup (v, e) -> c_call "DUP" [(varname v); (expr_to_c (level + 1) e)]
+  | `EmptyPrototype _ -> "&empty_prototype"
+  | `ExternalCall (fn, args, _, _, _) ->
+    c_call (fst fn) (List.map (expr_to_c (level + 1)) args)
+
+  | `FloatLiteral (f, _) -> c_call "qdbp_float" [string_of_float f]
+  | `IntLiteral (i, _) -> c_call "qdbp_int" [string_of_int i]
+  (* FIXME: *)
+  | `StringLiteral (s, _) -> c_call "qdbp_string" [quoted s]
+
+  | `MethodInvocation (receiver, label, args, _, _, _) ->
+    let args_c = List.map (fun (_, e, _) -> expr_to_c (level + 1) e) args in
+    let label_c = string_of_int (fst label) in
+    let receiver_c = expr_to_c (level + 1) receiver in
+    c_call (invoke_fn_name (List.length args))
+      (receiver_c :: label_c :: args_c)
+  | `PatternMatch (v, cases, _, _, _) ->
+    let rec cases_to_c level cases =
+      match cases with
+      | [] -> "match_failed()"
+      | ((tag, _), ((arg, _), body, _), _) :: rest ->
+        c_call "MATCH" [
+          ("tag");
+          (string_of_int tag);
+          (varname arg);
+          expr_to_c (level + 1) body;
+          (newline level) ^ (cases_to_c (level + 1) rest)
+        ]
+    in
+
+    paren ((c_call "decompose_variant" [(varname v); "&tag"; "&payload"]) ^ "," ^ (newline level)^
+    cases_to_c level cases)
+  | `PrototypeCopy (ext, ((label, _), (meth_id, meth_fvs), _), _, op, _, _) ->
+    let ext_c = expr_to_c (level + 1) ext in
+    let label_c = string_of_int label in
+    let fn = match op with
+      | `Extend ->
+        "extend"
+      | `Replace ->
+        "replace"
+    in
+    let code_ptr = method_name meth_id in 
+    let fv_list = List.map varname (FvSet.elements meth_fvs) in
+    let captures_c =
+      if (List.length fv_list) > 0 then
+        (paren ("struct qdbp_object*" ^ (brace (string_of_int (List.length fv_list))))) ^bracket (String.concat ", " fv_list) 
+      else "NULL"
+    in
+    c_call fn [
+      ext_c;
+      label_c;
+      code_ptr;
+      captures_c;
+      string_of_int (List.length fv_list)
+    ]
+  | `TaggedObject ((tag, _), value, _, _, _) ->
+    c_call "variant_create" [
+      (string_of_int tag);
+      expr_to_c (level + 1) value
+    ]
+  | `VariableLookup (v, _, _) ->
+    varname v
+
+let fn_definitions methods =
+  let fn_definition (id, (args, body, _, fvs, bvs)) =
+    let declare id =
+      (indent 1) ^ "qdbp_object_ptr " ^ (varname id) ^ ";" in
+    let init_fv idx id =
+      (indent 1) ^ 
+      "qdbp_object_ptr " ^ (varname id) ^
+      " = (captures" ^ (brace (string_of_int idx)) ^ ");"
+    in
+    let bvs_declarations = List.map declare (VarSet.elements bvs) in
+    let fvs_initializations = List.mapi init_fv (FvSet.elements fvs) in
+    (fn_sig id args) ^ " " ^ (bracket ( "\n" ^
+                                        (String.concat "\n" bvs_declarations) ^ "\n" ^
+                                        (String.concat "\n" fvs_initializations) ^ "\n" ^
+                                        newline 1 ^ "tag_t tag; qdbp_object_ptr payload;" ^
+                                        newline 1 ^ ("return ") ^
+                                        (expr_to_c 1 body) ^ ";"))
+  in
+  let method_strs = List.map fn_definition (IntMap.bindings methods) in
+  String.concat "\n" method_strs
+
+
+let codegen_c methods main_method_id =
+  let main_method = method_name main_method_id in
+  {|#include "runtime.h"
+#include "basic_fns.h"
+|} ^
+  invoke_fns methods ^ "\n" ^
+  fn_declarations methods ^ "\n" ^
+  fn_definitions methods ^ "\n" ^
+  "int main() {qdbp_object_ptr result = " ^ main_method ^ "(NULL); drop(result); print_refcounts(); return 0;}"
